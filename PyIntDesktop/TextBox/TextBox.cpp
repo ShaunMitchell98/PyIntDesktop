abd42faca51f4445d19400dffc576c06c21875bb
Shaun Mitchell <s.mitchell1998@hotmail.co.uk>
2020-12-14 14:34:42 +0000

    Made input textbox multiline.

--- PyIntDesktop/TextBox/TextBox.cpp
+++ PyIntDesktop/TextBox/TextBox.cpp
@@ -2,7 +2,7 @@
 
 HWND TextBox::InitWindow(HINSTANCE hInstance, HWND parentWindow, int x, int y)
 {
-    HWND textBox = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT | ES_AUTOHSCROLL | ES_WANTRETURN,
+    HWND textBox = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_MULTILINE | ES_LEFT | ES_WANTRETURN | ES_AUTOVSCROLL,
         x, y, 700, 100, parentWindow, nullptr, hInstance, 0);
 
     if (!textBox)
